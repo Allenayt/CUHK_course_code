2cef45c58fa789305e6658b467a8941fcb0ddd13
AN Yuting <allenayt1997@gmail.com>
2020-02-20 13:09:45 +0800

    modified

--- MAEG 5080 Smart Materials/Assignment 1/A1Q2.m
+++ MAEG 5080 Smart Materials/Assignment 1/A1Q2.m
@@ -6,12 +6,8 @@ zeta = [0 0.1 1 5];
 for i = 1:4
     num = [0 x0 v0+2*zeta(i)*omega*x0];
     den = [1 2*zeta(i)*omega omega^2];
-    [A, B, C, D] = tf2ss(num,den);
-    %sys = ss(A, B, C, D)
-    %x0 = [0.002, 0.0012]
     t = 0:0.01:8;
     impulse(num, den, t)
-    %initial(sys, t)
     grid on 
     hold on
 end
