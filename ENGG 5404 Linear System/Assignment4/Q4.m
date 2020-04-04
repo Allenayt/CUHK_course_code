3cac893614ba1b2c621ef61ec36d6483225ba01f
AN Yuting <allenayt1997@gmail.com>
2020-04-04 20:09:46 +0800

    Assignment 4

--- ENGG 5404 Linear System/Assignment4/Q4.m
+++ ENGG 5404 Linear System/Assignment4/Q4.m
@@ -1,7 +1,7 @@
-plot(out.tout, out.LQG(:,1), 'LineWidth', 1.5)
+plot(out.tout, out.LQG(:,1), 'LineWidth', 1.2)
 hold on
-plot(out.tout, out.LQG(:,2), 'LineWidth', 1.5)
+plot(out.tout, out.LQG(:,2), 'LineWidth', 1.2)
 hold on
 legend('x_{1}', 'x_{1}dot')
-axis([0 6 -0.6 1.2])
+axis([0 15 -0.6 1.2])
 grid on
